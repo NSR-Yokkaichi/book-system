358a5e9bfe7831f17a4ceaf3ef2d0eb1ae4ab5c9
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-02-09 10:35:46 +0900

    サインイン画面の作成

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,22 +1,5 @@
-"use client";
-import { authClient } from "@/lib/auth-client";
+import SignIn from "@/components/mui-templates/SignIn";
 
 export default async function Home() {
-  return (
-    <form
-      onSubmit={async (e) => {
-        e.preventDefault();
-        await authClient.signIn.social({
-          provider: "google",
-        });
-      }}
-    >
-      <button
-        type="submit"
-        className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-      >
-        Sign in with Google
-      </button>
-    </form>
-  );
+  return <SignIn />;
 }
