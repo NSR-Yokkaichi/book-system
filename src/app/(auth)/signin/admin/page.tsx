a99da7d39242e84b3664cafe6abbd898db5271c3
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-03-09 08:51:23 +0900

    feat: adminサインインページに動的レンダリングを追加

--- src/app/(auth)/signin/admin/page.tsx
+++ src/app/(auth)/signin/admin/page.tsx
@@ -1,6 +1,8 @@
 import { SignInWithPassword } from "@/components/mui-templates/SignIn";
 import prisma from "@/lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 export default async function SignInAdmin() {
   const userCount = await prisma.user.count({ where: { role: "admin" } });
   return <SignInWithPassword isFirstAccount={userCount === 0} />;
