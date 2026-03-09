1ffd8a4e3790a62a2b97a046d589d6119b641f26
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-03-09 09:35:21 +0900

    miss

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,5 @@
 import { adminClient, usernameClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
   plugins: [usernameClient(), adminClient()],
 });
