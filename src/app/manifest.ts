3080d0f46f27304a0eedaa6d13f3bea43243a9f0
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-04-02 23:45:20 +0900

    feat: 192pxを追加

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -15,7 +15,12 @@ export default function manifest(): MetadataRoute.Manifest {
     id: "https://book.unipro-n.com/",
     icons: [
       {
-        src: "/icon.png",
+        src: "/icon_192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon_512.png",
         sizes: "512x512",
         type: "image/png",
       },
