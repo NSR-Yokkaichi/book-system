9663e15340606fa65e1b9f4230520256796489f5
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-04-02 11:47:42 +0900

    format

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,10 @@
+import { passkey } from "@better-auth/passkey";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { admin, haveIBeenPwned, username } from "better-auth/plugins";
 // If your Prisma file is located elsewhere, you can change the path
 import prisma from "@/lib/prisma";
 import { transporter } from "./email";
-import { passkey } from "@better-auth/passkey";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
