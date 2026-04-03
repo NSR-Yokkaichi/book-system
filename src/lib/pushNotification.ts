87b59e11ab2ff5ef86022633bb3d59668b320e6f
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-04-03 22:32:12 +0900

    miss

--- src/lib/pushNotification.ts
+++ src/lib/pushNotification.ts
@@ -8,7 +8,7 @@ import prisma from "./prisma";
 
 webpush.setVapidDetails(
   "mailto:webmaster@uniproject.jp",
-  process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!,
+  process.env.VAPID_PUBLIC_KEY!,
   process.env.VAPID_PRIVATE_KEY!,
 );
 
