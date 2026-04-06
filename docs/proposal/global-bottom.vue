e800a37e2913921af62f073319353a7b485b0ffb
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-04-06 10:12:12 +0900

    提案書を追記

---
+++ docs/proposal/global-bottom.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer
+    class="absolute bottom-0 left-0 right-0 p-2 text-sm text-center text-gray-500"
+  >
+    {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}
+  </footer>
+</template>
